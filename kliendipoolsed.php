d5b8a0dd242b1ce2dc0213c94a230e9972e4d185
Martin Meltsas <martin.meltsas@khk.ee>
2015-09-17 14:48:31 +0300

    6. jQuery kass koeraks [Delivers #103581916]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,21 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>Martini PseudoProjekt</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
-<script>
-    function changeImg() {
-        var pic = document.getElementById('pilt');
-        if (pic.name == "KASS") {
-            pic.src="http://www.dog-boarding-wetherby.co.uk/wp-content/uploads/How-to-choose-a-healthy-dog-2.jpg";
-            pic.name="KOER";
-        }
-        else {
-            pic.src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg";
-            pic.name="KASS";
-        }
-    }
-</script>
+
 <body>
+<script>
+    //    function changeImg() {
+    //        var pic = document.getElementById('pilt');
+    //        if (pic.name == "KASS") {
+    //            pic.src="http://www.dog-boarding-wetherby.co.uk/wp-content/uploads/How-to-choose-a-healthy-dog-2.jpg";
+    //            pic.name="KOER";
+    //        }
+    //        else {
+    //            pic.src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg";
+    //            pic.name="KASS";
+    //        }
+    //    }
+    $(document).ready(function() {
+
+        $('#pilt').click(function() {
+            // console.log("tere!!!!!!");
+            $('#pilt').attr('src', "http://www.dog-boarding-wetherby.co.uk/wp-content/uploads/How-to-choose-a-healthy-dog-2.jpg");
+        });
+    });
+</script>
+
 <button onclick="alert('tere maailm!!');">Tere maailm</button>
 <div>
     <a onclick="alert('tere maailm!!');" href="http://khk.ee/">tere maailm</a>
@@ -27,7 +37,8 @@
     <a href="" onclick="alert('jääme siia');">jääme siia</a>
 </div>
 
-<img id='pilt' name="KASS" onclick="changeImg()" src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg">
+<!--<img id='pilt' name="KASS" onclick="changeImg()" src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg">-->
+<img id='pilt' name="KASS" src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg" alt="Looma Pilt">
 
 </body>
 </html>
