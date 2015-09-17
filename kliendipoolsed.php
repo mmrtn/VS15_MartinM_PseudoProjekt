df39c309d445e906986394fd692fbdb7ec110729
Martin Meltsas <martin.meltsas@khk.ee>
2015-09-17 14:17:16 +0300

    5. Kass koeraks [Delivers #103581894]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -4,6 +4,19 @@
     <meta charset="UTF-8">
     <title>Martini PseudoProjekt</title>
 </head>
+<script>
+    function changeImg() {
+        var pic = document.getElementById('pilt');
+        if (pic.name == "KASS") {
+            pic.src="http://www.dog-boarding-wetherby.co.uk/wp-content/uploads/How-to-choose-a-healthy-dog-2.jpg";
+            pic.name="KOER";
+        }
+        else {
+            pic.src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg";
+            pic.name="KASS";
+        }
+    }
+</script>
 <body>
 <button onclick="alert('tere maailm!!');">Tere maailm</button>
 <div>
@@ -13,6 +26,8 @@
 <div>
     <a href="" onclick="alert('jääme siia');">jääme siia</a>
 </div>
+
+<img id='pilt' name="KASS" onclick="changeImg()" src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg">
 
 </body>
 </html>
