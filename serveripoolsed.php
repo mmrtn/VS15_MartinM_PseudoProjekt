420de97674c053e1531b1e4c65b06195a440b6bc
Martin Meltsas <martin.meltsas@khk.ee>
2015-09-23 14:58:57 +0300

    14. Täisarvud [Delivers #103975844]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Pseudoprojekt Serveripoolsed</title>
+    <title>Serveripoolsed Meltsas</title>
 </head>
 <body>
     <?php
+        $age = 99;
         $first='Martin';
         $last='Meltsas';
-        echo "$first $last";
+        echo "$first $last ($age)";
     ?>
 
 </body>
