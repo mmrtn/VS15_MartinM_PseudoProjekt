fab953d5487cd7ad01123db38b6e441015aa5b25
Martin Meltsas <martin.meltsas@khk.ee>
2015-09-23 20:28:20 +0300

    22. $_POST [Delivers #103975868]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,44 +6,18 @@
 </head>
 <body>
     <?php
-        function is_first_letter_vowel($str) {
-            return (strpos('aeiouõäüö', strtolower($str[0])) !== FALSE) ? true : false;
+        if (!empty($_POST['kass'])) {
+            echo "Kass oli: ".$_POST['kass'];
         }
-
-        $age = 99;
-        $first='Martin';
-        $last='Meltsas';
-
-        $float1=1.23;
-        $float2=2.34;
-
-        $isik=array(
-            'eesnimi'=>$first,
-            'perenimi'=>$last,
-            'vanus'=>$age
-
-        );
-
-    $isik_objekt = (object) array_merge($isik, array('sugu'=>'mees'));
-
-    $eesnimi_algab_vokaaliga=is_first_letter_vowel($isik_objekt->eesnimi);
-    // echo "<em>ALGAB VOKAALIGA: $eesnimi_algab_vokaaliga</em>";
-
-    foreach ($isik_objekt as $key=>$value) {
-        echo "<p>$key : $value</p>";
-    }
-
-    echo "<p>Teha kaks muutujat väärtustega 1.23 ja 2.34 ning kuvada ekraanil nende summat : </p>" , ($float1+$float2);
-
-    if (!empty($_GET['koer'])) {
-        echo "<p>".$_GET['koer']."</p>";
-    }
-
-//    echo $isik['eesnimi'].' '.$isik['perenimi'].' ('.$isik['vanus'].')';
-//    if ((strpos('aeiouõäüö', strtolower($isik['eesnimi'][0])) !== FALSE)) {
-//        echo '<br>Nimi algab vokaaliga';
-//    }
     ?>
+
+    <form action="?" method="post">
+
+        Name  <input name="kass"><br>
+
+        <input type="submit" value="?">
+
+    </form>
 
 </body>
 </html>
